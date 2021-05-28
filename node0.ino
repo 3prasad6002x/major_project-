6c9b60575cd5b1e3583669a40a2e25c2113ea74f
ELLABOINA SAI PRASAD <61202805+3prasad6002x@users.noreply.github.com>
2021-05-28 19:53:14 +0530

    Add files via upload

--- node0.ino
+++ node0.ino
@@ -9,8 +9,8 @@
  #define CE_PIN 2
  #define CSN_PIN 4
  #define INTERVAL_MESSAGE1 2100
- #define INTERVAL_MESSAGE2 2700
- #define INTERVAL_MESSAGE4 3200
+ #define INTERVAL_MESSAGE2 3300
+ #define INTERVAL_MESSAGE4 4000
  
 const char* ssid = "HOME N/W";
 const char* pass = "Saiprasad@333";
@@ -18,12 +18,13 @@ unsigned long time_1 = 0;
 unsigned long time_2 = 0;
 unsigned long time_4 = 0;  
 
+int x,soil_m1,soil_m2;
 BlynkTimer timer;
 
 const uint64_t receive_node_1 = 0xE8E8F0F0E1AA;
 const uint64_t receive_node_2 = 0xE8E8F0F0E1EE;
 
-char auth[] = "2hl1ihl8mnQcbLKc6M46qv5q6PymnfKj";
+char auth[] = "oBSuD77K9-4_5bEss0x1HBRcUZiHDpiX";
 
  
 RF24 radio(CE_PIN, CSN_PIN);
@@ -70,17 +71,29 @@ float sensor_data[4];
   if(millis() > time_4 + INTERVAL_MESSAGE4){
         time_4 = millis();  
  Serial.println((String)sensor_data[0]+(String)sensor_data[1]+(String)sensor_data[2]+(String)sensor_data[3]);
+ x=(sensor_data[0]<100.00 && sensor_data[1] <50.00);
+ soil_m1=(sensor_data[2]>100.00 && sensor_data[2]<1054.00);
+ soil_m2=(sensor_data[3]>100.00 && sensor_data[3]<1054.00);
+ Serial.println(x);
+ Serial.println(soil_m1);
+ Serial.println(soil_m2);
+ if(x && soil_m1 &&soil_m2){
  Blynk.virtualWrite(V2, sensor_data[0]);
   Blynk.virtualWrite(V3, sensor_data[1]);
   Blynk.virtualWrite(V4, sensor_data[2]);
   Blynk.virtualWrite(V5, sensor_data[3]);
+  Serial.println("Success No redundancy data sent");
   //delay(10000);
+ }
+ else{
+  Serial.println("Redundancy data");
+  }
  }
  }
 void setup()
  {
  Serial.begin(1000000);
- Blynk.begin(auth, ssid, pass);
+ Blynk.begin(auth, ssid, pass, IPAddress(192,168,0,165), 8080);
  Serial.println("Serial begin...");
  radio.begin();
  Serial.print("rf begin...");
